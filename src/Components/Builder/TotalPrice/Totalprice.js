78e6bec6148d73f81ddf55c70c9057a0a3074dbf
Arup paul <aruppaulctg@gmail.com>
2021-05-27 16:42:37 +0600

    Total price component

---
+++ src/Components/Builder/TotalPrice/Totalprice.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './totalprice.module.css'
+const Totalprice = () => {
+    return (
+        <div className={classes.total}>
+            <div>Total Price</div>
+            <div>3000.00 Tk</div>
+        </div>
+    )
+}
+
+export default Totalprice;
