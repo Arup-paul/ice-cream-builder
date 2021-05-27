5e777c7669d755fa705f42ba8aa63b0dd4827abc
Arup paul <aruppaulctg@gmail.com>
2021-05-27 16:07:21 +0600

    layout main component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from "react";
 import './App.css';
 import Header from "./Components/Header/Header";
+import Layout from "./Components/layout/layout";
+import Body from "./Components/Body/Body";
+import Footer from "./Components/Footer/Footer";
 function App() {
   return (
-    <div className="App">
-       <Header />
-    </div>
+     <Layout>
+         <Header />
+         <Body />
+         <Footer />
+     </Layout>
   );
 }
 
